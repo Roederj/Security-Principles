49fe2f10a66ad8f6033a54facf3135702977323f
Roederj <roederjamie@gmail.com>
2017-01-30 07:05:07 -0600

    Add files via upload

---
+++ CyberPrinciples/Scripts/InfoHiding.js
@@ -0,0 +1,24 @@
+﻿
+window.onload = hideExplanations;
+
+// Function to hide goodCode on page load
+//function hideGoodCode() {
+//    var hideCode = document.getElementsByClassName("goodCode");
+
+//    for (var i = 0; i < hideCode.length; i++) {
+//        hideCode[i].style.visibility = "hidden";
+//    }
+
+//    //    window.alert(arr[0].textContent);
+//}
+
+// Hides Explanations on page load
+function hideExplanations() {
+    var hideDivs = document.getElementsByClassName("explainCode");
+
+    hideDivs[0].style.visibility = "hidden";
+}
+
+function showExplanation() {
+
+}
