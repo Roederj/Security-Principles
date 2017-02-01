47d3bc5cde0e05341f2293336a65d39e5cb640b1
JamieDesktop <roederjamie@gmail.com>
2017-01-31 23:00:19 -0600

    Started on showing appropriate explanations during mouse hovers on the Information Hiding page

--- CyberPrinciples/Scripts/InfoHiding.js
+++ CyberPrinciples/Scripts/InfoHiding.js
@@ -19,6 +19,30 @@ function hideExplanations() {
     hideDivs[0].style.visibility = "hidden";
 }
 
-function showExplanation() {
+//Shows explanations on mouse hover
+function showExplanation(id) {
+    var showDivs = document.getElementById("explainDefault");
 
+    window.alert(showDivs.textContent);
+
+    switch (id) {
+        case "bad1":
+            showDivs = document.getElementById("explainBad1");
+            break;
+        case "bad2":
+            showDivs = document.getElementById("explainBad2");
+            break;
+        case "good1":
+            showDivs = document.getElementById("explainGood1");
+            break;
+        case "good2":
+            showDivs = document.getElementById("explainGood2");
+            break;
+        case "good3":
+            showDivs = document.getElementById("explainGood3");
+            break;
+
+            window.alert(showDivs[0].textContent);
+            showDivs[0].style.visibility = "visible";
+    }
 }
