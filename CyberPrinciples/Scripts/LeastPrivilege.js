6fef6c705bede623978561344138f952d1ccd80d
JamieDesktop <roederjamie@gmail.com>
2017-03-15 16:47:06 -0500

    Styling on Home Fixes in progress on Least Privilege Started Resource Encapsulation

--- CyberPrinciples/Scripts/LeastPrivilege.js
+++ CyberPrinciples/Scripts/LeastPrivilege.js
@@ -17,43 +17,64 @@
 
 document.addEventListener("keydown", function (event) {
 
-    var movBox = document.getElementById("testDiv");
+    //var movBox = document.getElementById("testDiv");
 
+    //switch (event.which) {
+    //    case 37: //Left
+    //        if (!(movBox.style.left < (0 + 'px'))) {
+    //            movBox.style.left = (movBox.offsetLeft - 10) + 'px';
+    //        }
+    //        else if (movBox.style.left < (0 + 'px')) {
+    //            movBox.style.left = 0 +'px';
+    //        }
+    //        break;
+    //    case 38: //Up
+    //        if (!(movBox.style.top < (0 + 'px'))) {
+    //            movBox.style.top = (movBox.offsetTop - 10) + 'px';
+    //        }
+    //        else if (movBox.style.top < (0 + 'px')) {
+    //            movBox.style.top = (0 + 'px');
+    //        }
+    //        break;
+    //    case 39: //Right
+    //        if (!(movBox.style.left > (window.innerWidth + movBox.style.left + 'px'))) {
+    //            movBox.style.left = (movBox.offsetLeft + 10) + 'px';
+    //        }
+    //        else if (movBox.style.left > (window.innerWidth + movBox.style.left + 'px')) {
+    //            movBox.style.left = window.innerWidth - movBox.style.left + 'px';
+    //        }
+    //        break;
+    //    case 40: //Down
+    //        if (!(movBox.style.top > (window.innerHeight + movBox.style.left + 'px'))) {
+    //            movBox.style.top = (movBox.offsetTop + 10) + 'px';
+    //        }
+    //        else if (movBox.style.top > (window.innerHeight + movBox.style.left + 'px')) {
+    //            movBox.style.top = window.innerHeight - movBox.offsetHeight + 'px';
+    //        }
+    //        break;
+    //}
+    
+  
+    var movBox = document.getElementById("testDiv");
+    var limitBox = document.getElementById("limitDiv");
+//    window.alert(limitBox.offsetHeight);
     switch (event.which) {
         case 37: //Left
-            if (!(movBox.style.left < (0 + 'px'))) {
+            window.alert(movBox.style.left + 'px');
+            if (movBox.style.left > 0 + 'px') {
                 movBox.style.left = (movBox.offsetLeft - 10) + 'px';
-            }
-            else if (movBox.style.left < (0 + 'px')) {
-                movBox.style.left = 0 +'px';
             }
             break;
         case 38: //Up
-            if (!(movBox.style.top < (0 + 'px'))) {
-                movBox.style.top = (movBox.offsetTop - 10) + 'px';
-            }
-            else if (movBox.style.top < (0 + 'px')) {
-                movBox.style.top = (0 + 'px');
-            }
+
             break;
         case 39: //Right
-            if (!(movBox.style.left > (window.innerWidth + movBox.style.left + 'px'))) {
-                movBox.style.left = (movBox.offsetLeft + 10) + 'px';
-            }
-            else if (movBox.style.left > (window.innerWidth + movBox.style.left + 'px')) {
-                movBox.style.left = window.innerWidth - movBox.style.left + 'px';
-            }
+
             break;
         case 40: //Down
-            if (!(movBox.style.top > (window.innerHeight + movBox.style.left + 'px'))) {
-                movBox.style.top = (movBox.offsetTop + 10) + 'px';
-            }
-            else if (movBox.style.top > (window.innerHeight + movBox.style.left + 'px')) {
-                movBox.style.top = window.innerHeight - movBox.offsetHeight + 'px';
-            }
+
             break;
     }
-    
 
 
 
