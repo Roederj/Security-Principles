2c476486d0215211a992202124bdf82678d34003
JamieDesktop <roederjamie@gmail.com>
2017-05-05 00:45:17 -0500

    Some final updates and tweaks

--- CyberPrinciples/Scripts/Modularity.js
+++ CyberPrinciples/Scripts/Modularity.js
@@ -1,3 +1,25 @@
 ﻿// Add a script to highlight Navbar on hover
+function lightNavMenu() {
+    var navMenu = document.querySelectorAll("li");
+    for (var i = 0; i < navMenu.length; i++) {
+        navMenu[i].style.borderColor = "rgb(152, 230, 152)";
+    }
+}
+
+function returnNavMenu() {
+    var navMenu = document.querySelectorAll("li");
+    for (var i = 0; i < navMenu.length; i++) {
+        navMenu[i].style.borderColor = "black";
+    }
+}
 
 // Add a script to highlight Border on hover
+function lightBorder() {
+    var border = document.getElementById("bodyWrap");
+    border.style.borderColor = "rgb(152, 230, 152)";
+}
+
+function returnBorder() {
+    var border = document.getElementById("bodyWrap");
+    border.style.borderColor = "black";
+}
